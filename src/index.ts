d3e28f935a17123d83f42ae25134a12678789646
avon <dhaanddi@gmail.com>
2025-10-20 00:39:33 +0500

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { EventEmitter, EventStream, ListenerHandler } from './core';
+export { getArgs, compilePattern} from './helpers';
+export type { EventMap, EventKey, EventArgs, Listener, ListenerMeta, ListenerOptions } from './types';
